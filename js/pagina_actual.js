d7e706557738d91cce1d4150740562f3e8dfde22
juanluna96 <juandluna17@gmail.com>
2020-07-31 23:07:08 -0500

    V1.14 | Saber pagina actual JS

---
+++ js/pagina_actual.js
@@ -0,0 +1,18 @@
+/* ------------------ Objeto con propiedades de formulario ------------------ */
+var propPagina = {
+    ListaNavegacion: document.getElementsByClassName('navbar-nav')[0].children,
+    urlActual: window.location.pathname.slice(1)
+}
+
+/* -------------------- Objeto con metodos del formulario ------------------- */
+var metPagina = {
+    saber_pagina_actual: function() {
+        for (let i = 0; i < propPagina.ListaNavegacion.length; i++) {
+            if (propPagina.ListaNavegacion[i].children[0].getAttribute('href') == propPagina.urlActual) {
+                propPagina.ListaNavegacion[i].className = "active";
+            }
+
+        }
+    }
+}
+metPagina.saber_pagina_actual();
