357d094fb4c560fc05b945222d2c3baadbfb5a32
juanluna <juandluna17@gmail.com>
2020-08-01 12:10:11 -0500

    V1.145 | Saber pagina actual JS

--- js/pagina_actual.js
+++ js/pagina_actual.js
@@ -1,18 +1,22 @@
-/* ------------------ Objeto con propiedades de formulario ------------------ */
-var propPagina = {
-    ListaNavegacion: document.getElementsByClassName('navbar-nav')[0].children,
-    urlActual: window.location.pathname.slice(1)
-}
+(function() {
+    /* ------------------ Objeto con propiedades de formulario ------------------ */
+    var propPagina = {
+        ListaNavegacion: document.getElementsByClassName('navbar-nav')[0].children,
+        urlActual: window.location.pathname.slice(1)
+    }
 
-/* -------------------- Objeto con metodos del formulario ------------------- */
-var metPagina = {
-    saber_pagina_actual: function() {
-        for (let i = 0; i < propPagina.ListaNavegacion.length; i++) {
-            if (propPagina.ListaNavegacion[i].children[0].getAttribute('href') == propPagina.urlActual) {
-                propPagina.ListaNavegacion[i].className = "active";
+    /* -------------------- Objeto con metodos del formulario ------------------- */
+    var metPagina = {
+        saber_pagina_actual: function() {
+            for (let i = 0; i < propPagina.ListaNavegacion.length; i++) {
+                if (propPagina.ListaNavegacion[i].children[0].getAttribute('href') == propPagina.urlActual) {
+                    propPagina.ListaNavegacion[i].className = "nav-item active";
+                } else {
+                    propPagina.ListaNavegacion[i].className = "nav-item";
+                }
+
             }
-
         }
     }
-}
-metPagina.saber_pagina_actual();
+    metPagina.saber_pagina_actual();
+})()
