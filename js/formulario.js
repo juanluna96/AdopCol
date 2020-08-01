18f786da0958c6491df118b4844f1182df8ee28c
juanluna96 <juandluna17@gmail.com>
2020-07-31 23:06:37 -0500

    V1.13 | Validacion del formulario de contacto

--- js/formulario.js
+++ js/formulario.js
@@ -1,30 +1,53 @@
-(function() {
-    /* ------------------ Objeto con propiedades de formulario ------------------ */
-    var propFormulario = {
-        formulario: document.formulario_contacto,
-        elementos: document.formulario_contacto.elements
-    }
+/* ------------------ Objeto con propiedades de formulario ------------------ */
+var propFormulario = {
+    formulario: document.formulario_contacto,
+    elementos: document.formulario_contacto.elements,
+    error: null,
+    textoError: null
+}
 
-    /* -------------------- Objeto con metodos del formulario ------------------- */
-    var metFormulario = {
-        inicio: function() {
-            for (let i = 0; i < propFormulario.elementos.length; i++) {
-                if (propFormulario.elementos[i].type == 'text' || propFormulario.elementos[i].type == 'email' || propFormulario.elementos[i].nodeName.toLowerCase() == 'textarea') {
-                    propFormulario.elementos[i].addEventListener('focus', metFormulario.focusInput);
-                    propFormulario.elementos[i].addEventListener('blur', metFormulario.blurInput);
-                }
-            }
-        },
-        focusInput: function() {
-            this.parentElement.children[1].className = 'label active';
-        },
-        blurInput: function() {
-            if (this.value == "") {
-                this.parentElement.children[1].className = 'label';
+/* -------------------- Objeto con metodos del formulario ------------------- */
+var metFormulario = {
+    inicio: function() {
+        for (let i = 0; i < propFormulario.elementos.length; i++) {
+            if (propFormulario.elementos[i].type == 'text' || propFormulario.elementos[i].type == 'email' || propFormulario.elementos[i].nodeName.toLowerCase() == 'textarea') {
+                propFormulario.elementos[i].addEventListener('focus', metFormulario.focusInput);
+                propFormulario.elementos[i].addEventListener('blur', metFormulario.blurInput);
             }
         }
+        propFormulario.formulario.addEventListener('submit', metFormulario.validarInputs);
+    },
+    focusInput: function() {
+        this.parentElement.children[1].className = 'label active';
+    },
+    blurInput: function() {
+        if (this.value == "") {
+            this.parentElement.children[1].className = 'label';
+        }
+    },
+    validarInputs: function(e) {
+        for (let i = 0; i < propFormulario.elementos.length; i++) {
+            if (propFormulario.elementos[i].value == '') {
+                e.preventDefault();
 
+                if (propFormulario.elementos[i].parentElement.children.length < 3) {
+                    propFormulario.error = document.createElement('p');
+                    propFormulario.textoError = document.createTextNode('Porfavor llena el campo con tu ' + propFormulario.elementos[i].name);
+                    propFormulario.error.appendChild(propFormulario.textoError);
+                    propFormulario.error.className = 'error';
+
+                    propFormulario.elementos[i].parentElement.appendChild(propFormulario.error);
+                }
+            } else {
+                if (propFormulario.elementos[i].parentElement.children.length >= 3) {
+                    propFormulario.error = propFormulario.elementos[i].parentElement.getElementsByTagName('p')[0];
+                    propFormulario.elementos[i].parentElement.removeChild(propFormulario.error);
+                }
+            }
+
+        }
     }
 
-    metFormulario.inicio();
-})()
+}
+
+metFormulario.inicio();
